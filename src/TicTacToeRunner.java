c3e0dbc7a1502290cf63fb7f7d3fa7cda41bb221
Bergerj2 <bergerj2@mail.uc.edu>
2025-10-06 19:52:57 -0400

    final commit

---
+++ src/TicTacToeRunner.java
@@ -0,0 +1,9 @@
+import javax.swing.SwingUtilities;
+
+public class TicTacToeRunner {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new TicTacToeFrame();
+        });
+    }
+}
